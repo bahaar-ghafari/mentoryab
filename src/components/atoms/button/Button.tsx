576ffab7895453109a0df28c5bb221ef5fccc76d
mahsa <mahsafaateh@gmail.com>
2026-06-03 21:45:45 +0200

    add: button

--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -1,1 +1,7 @@
-//TODO: !!!
+import { Button as MUIButton } from '@mui/material';
+
+const Button = ({ title }: { title: string }) => {
+  return <MUIButton variant="contained">{title}</MUIButton>;
+};
+
+export default Button;
