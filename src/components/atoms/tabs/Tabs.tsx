6ae392fb32f38a0806c907917c1a09eb9bfeb660
mahsa <mahsafaateh@gmail.com>
2026-06-05 11:49:42 +0200

    changes on Tabs attributes

--- src/components/atoms/tabs/Tabs.tsx
+++ src/components/atoms/tabs/Tabs.tsx
@@ -2,11 +2,22 @@ import { Tabs as MUITabs } from '@mui/material';
 
 type TabsProps = {
   value: string | number;
-  onChange: () => void;
+  onChange: (event: React.SyntheticEvent, newValue: string | number) => void;
+  children: React.ReactNode;
 };
 
-const Tabs = ({ value, onChange }: TabsProps) => {
-  return <MUITabs value={value} onChange={onChange} />;
+const Tabs = ({ value, onChange, children }: TabsProps) => {
+  return (
+    <MUITabs
+      value={value}
+      onChange={onChange}
+      textColor="secondary"
+      indicatorColor="secondary"
+      aria-label="mentee mentor tabs"
+    >
+      {children}
+    </MUITabs>
+  );
 };
 
 export default Tabs;
